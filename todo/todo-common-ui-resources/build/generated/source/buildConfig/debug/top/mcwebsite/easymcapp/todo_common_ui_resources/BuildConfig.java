acaaed37f5ce665d20eb7f5e1066bc898b9b64ca
woxin123 <1293141942@qq.com>
2022-01-23 21:48:57 +0800

    fix(novel): fix chapter not update & opti code

---
+++ todo/todo-common-ui-resources/build/generated/source/buildConfig/debug/top/mcwebsite/easymcapp/todo_common_ui_resources/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package top.mcwebsite.easymcapp.todo_common_ui_resources;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "top.mcwebsite.easymcapp.todo_common_ui_resources";
+  public static final String BUILD_TYPE = "debug";
+}
